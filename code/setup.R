45a6eadabb632a96dc156918d8e80a13cc504b0a
AdubIC <aweigel.ic@gmail.com>
2024-11-08 07:43:51 -0600

    new files for module 5

---
+++ code/setup.R
@@ -0,0 +1,24 @@
+## @knitr setup
+knitr::opts_chunk$set(echo = FALSE)
+knitr::opts_chunk$set(warning = FALSE)
+knitr::opts_chunk$set(message = FALSE)
+
+# set any missing NAs in tables to blank
+options(knitr.kable.NA = '')
+
+# load R packages
+library(fivethirtyeight)
+library(tidyverse)
+library(knitr)
+library(kableExtra)
+library(ggthemes)
+
+## @knitr loadData
+
+# load weather_check dataset
+data("weather_check", package="fivethirtyeight")
+
+# create subset, no missing NAs
+# pick a specific region
+sdat <- weather_check |>
+  filter(region==params$region)
